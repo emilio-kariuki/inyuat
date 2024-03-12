a84ead14b8ef9a45c21c15e6c52475a4b62c5588
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-03-13 00:11:50 +0300

    fix: fixes the export error

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,7 @@
 
 import MainHeader from "@/components/shared/main_header";
 import Image from "next/image";
+import { Hero } from "./hero";
 
 export default function Home() {
   return (
@@ -11,33 +12,4 @@ export default function Home() {
   );
 }
 
-export function Hero() {
-  return (
-    <section>
-      <div className="flex flex-col justify-center items-center mt-28">
-        <h2 className="text-[45px] font-normal max-w-[980px] text-center">
-          Founded in <span className="text-green-800 font-semibold">2010</span>{" "}
-          with the goal of providing the best quality fruits and vegetables.
-        </h2>
-        <h4 className="text-[22px] font-normal max-w-2xl text-center mt-7 text-[#677085]">
-          We are a family-owned business that is dedicated to providing our
-          customers with the freshest.
-        </h4>
-        <DiscoverSection />
-      </div>
-    </section>
-  );
-}
 
-export function DiscoverSection() {
-  return (
-    <div className="flex gap-4 mt-8" > 
-      <div className="bg-[#C1FB6B] p-4 rounded-[10px]"> 
-        <p  className="text-[14px] font-medium">Discover Our products</p>
-      </div>
-      <div className="bg-white p-4 rounded-[10px]">
-        <p className="text-[14px] font-medium">Learn more</p>
-      </div>
-    </div>
-  );
-}
