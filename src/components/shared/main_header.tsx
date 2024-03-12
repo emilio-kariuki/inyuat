309c35c7dca106da480bd00120c21686bbfb6855
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-03-13 00:02:20 +0300

    add: adds the main header' '

---
+++ src/components/shared/main_header.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { Mail } from "lucide-react";
+export default function MainHeader() {
+  return (
+    <div className="flex items-center mx-[30px] my-[20px] justify-between bg-[#EDF3F2]">
+      <Logo />
+      <RouteSection />
+      <ContactButton />
+    </div>
+  );
+}
+
+export function Logo() {
+  return (
+    <div className="flex items-center">
+      <Image
+        src={logo}
+        alt="logo"
+        className="h-[40px] w-[40px] rounded-full mr-[10px]"
+      />
+      <h3 className="font-bold text-[21px]">
+        INYUAT <span className="text-[#16A349]">FARM FRESH</span>
+      </h3>
+    </div>
+  );
+}
+
+export function RouteSection() {
+  const routes = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Products",
+      path: "/products",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "FAQ",
+      path: "/faq",
+    },
+  ];
+  return (
+    <div className="flex gap-5">
+      {routes.map((route, index) => {
+        return (
+          <div key={index} className=" hover:bg-green-800 px-5 py-2 rounded-[40px] hover:text-white">
+            <h3 className="font-medium hover:text-white">{route.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export function ContactButton() {
+  return (
+    <div className="flex bg-green-800 rounded-[13px] py-[15px] px-[20px] gap-4 items-center">
+      <h5 className="font-medium text-[14px] text-white">Contact Us</h5>
+      <Mail color="#ffffff" className="h-[18px] w-[18px] " />
+    </div>
+  );
+}
