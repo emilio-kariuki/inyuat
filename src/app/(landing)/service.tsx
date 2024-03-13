1db5658eea16c1cdaa528c6906752a07e6b9f250
Amanda AFA <marssarek@gmail.com>
2024-03-13 23:33:48 +0300

    add: adds new files

--- src/app/(landing)/service.tsx
+++ src/app/(landing)/service.tsx
@@ -6,8 +6,9 @@ export function ServiceSection(){
     return(
         <Element name="service">
             <section>
-                <div className="flex bg-[#004D3F] p-16 w-full items-center justify-center">
-                    <h3 className={`font-semibold text-[40px] text-white ${monasans.className}`}>We offer a wide range of products</h3>
+                <div className="flex flex-col bg-[#004D3F] p-16 w-full items-center justify-center">
+                    <h3 className={`font-medium text-[35px] text-white ${monasans.className}`}>We offer a wide range of products</h3>
+                    <h3 className={`text-[15px] font-normal max-w-3xl text-center gap-3 text-[#f9fAfA] ${monasans.className}`}>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
 
                 </div>
 
@@ -15,5 +16,16 @@ export function ServiceSection(){
 
         </Element>
         
+    );
+}
+
+function ServiceContainer(){
+    return(
+        <div className="flex flex-col">
+          <div className="rounded-full ">
+           
+          </div>
+        </div>
+
     );
 }
